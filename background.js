39cfc87940d13125e12467d9093aafeede2f0566
chiudesu <chiudesu@gmail.com>
2016-05-13 17:40:15 +0900

    executeScriptによって通信を解決、ページにsvgが埋まっているのも確認した TODO： 1,svgがアニメーションしているはずだが見えない 2,httpsなど多くのサイトはsvg挿入がはじかれる

--- background.js
+++ background.js
@@ -1,52 +1,89 @@
 var isSparkling = false;
 
+//アクティブなタブが変わったら呼び出し
+function updateTab() {
+  console.log("updateTab");
+  //TODO:タブにスクリプトが読み込まれているかどうかを確認してアイコンの状態を変える
+
+}
+
+//アイコンクリックで呼び出し
 function updateState(tab) {
-    isSparkling = !isSparkling;
-    if (isSparkling) {
-        //開始
-        chrome.browserAction.setIcon({
-            path: {
-                "19": "icons/enable19.png",
-                "38": "icons/enable38.png"
-            }
-        });
-        chrome.tabs.query( //現在選択中のtab.idが必要
-            {
-                active: true,
-                currentWindow: true
-            },
-            function(tabs) {
-                chrome.tabs.sendMessage(
-                    tabs[0].id, {
-                        type: "PrismSparkle_start"
-                    },
-                    function(res) {
-                        console.log("started:" + res); //レスポンスが返ってきた
-                    });
-            });
-    } else {
-        //停止
-        chrome.browserAction.setIcon({
-            path: {
-                "19": "icons/wait19.png",
-                "38": "icons/wait38.png"
-            }
-        });
-        chrome.tabs.query( //現在選択中のtab.idが必要
-            {
-                active: true,
-                currentWindow: true
-            },
-            function(tabs) {
-                chrome.tabs.sendMessage(
-                    tabs[0].id, {
-                        type: "PrismSparkle_stop"
-                    },
-                    function(res) {
-                        console.log("stopped:" + res); //レスポンスが返ってきた
-                    });
-            });
+  console.log("updateState");
+
+  isSparkling = !isSparkling;
+  if (isSparkling) {
+    //開始
+    chrome.browserAction.setIcon({
+      path: {
+        "19": "icons/enable19.png",
+        "38": "icons/enable38.png"
+      }
+    });
+    ensureSendMessage({
+        type: "PrismSparkle_start"
+      },
+      function(res) {
+        console.log("started:" + res); //レスポンスが返ってきた
+      });
+  } else {
+    //停止
+    chrome.browserAction.setIcon({
+      path: {
+        "19": "icons/wait19.png",
+        "38": "icons/wait38.png"
+      }
+    });
+    ensureSendMessage({
+        type: "PrismSparkle_stop"
+      },
+      function(res) {
+        console.log("stopped:" + res); //レスポンスが返ってきた
+      });
+  }
+}
+
+//初めてのtabで実行されたらまずスクリプトを流し込む
+function initContent(tabId, completedf) {
+  console.log("init");
+  chrome.tabs.executeScript(tabId, {
+    file: "jquery-2.2.3.min.js"
+  });
+  chrome.tabs.executeScript(tabId, {
+    file: "content.js"
+  }, function() {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+      throw Error("Unable to inject script into tab " + tabId);
     }
+
+    completedf();
+  });
+}
+
+//メッセージを送る
+//失敗したら（スクリプトがまだ読み込まれていないなら）initContentを呼ぶ
+function ensureSendMessage(message, callback) {
+  chrome.tabs.query( //現在選択中のtab.idが必要
+    {
+      active: true,
+      currentWindow: true
+    },
+    function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+        if (response && response.success) { //content側で受信したら、success:trueとされる
+          //成功
+          callback(response);
+
+        } else {
+          //失敗
+          initContent(tabs[0].id, function() {
+            chrome.tabs.sendMessage(tabs[0].id, message, callback);
+          });
+        }
+      });
+    });
 }
 
 chrome.browserAction.onClicked.addListener(updateState);
+chrome.tabs.onActivated.addListener(updateTab);
--- content.js
+++ content.js
@@ -9,56 +9,63 @@ var circle_R_MAX = 50.0;
 
 //毎フレーム呼び出される
 function sparkle() {
-    cntf += 1000.0 / FPS;
-    $circles.each(function(i, $elm) {
-        $elm.attr("r", (circle_R_MAX * cntf / DUR));
-        $elm.attr("stroke-opacity", Math.sin(Math.PI * cntf / DUR) / 3);
+  cntf += 1000.0 / FPS;
+  $circles.each(function(i, $elm) {
+    $elm.attr("r", (circle_R_MAX * cntf / DUR));
+    $elm.attr("stroke-opacity", Math.sin(Math.PI * cntf / DUR) / 3);
 
-        if (cntf < FADE) {
-            $elm.attr("opacity", cntf / FADE);
-        } else if (cntf > DUR - FADE) {
-            $elm.attr("opacity", (DUR - cntf) / FADE);
-        } else {
-            $elm.attr("opacity", 1);
-        }
-
-    });
-    if (cntf > DUR) { //cntf: 1000/FPS to DUR
-        cntf = 0;
+    if (cntf < FADE) {
+      $elm.attr("opacity", cntf / FADE);
+    } else if (cntf > DUR - FADE) {
+      $elm.attr("opacity", (DUR - cntf) / FADE);
+    } else {
+      $elm.attr("opacity", 1);
     }
+
+  });
+  if (cntf > DUR) { //cntf: 1000/FPS to DUR
+    console.log("Sparkle:reset cntf");
+    cntf = 0;
+  }
 }
 
 function startSparkle() {
-    //TODO 流し込み
-    //<script type="text/javascript" src="jquery-2.2.3.min.js"></script>
-    //<object class="PrismSparkle_circle" type="image/svg+xml" data="prismcircle.svg" width="300" height="300" style="position:absolute; left:200px; top:200px;"> </object>
-    $("body").prepend("<object class=\"PrismSparkle_circle\" type=\"image/svg+xml\" data=\"prismcircle.svg\" width=\"300\" height=\"300\" style=\"position:absolute; left:200px; top:200px;\"> </object>");
+  console.log("startSparkle");
+  //TODO 流し込み
+  //<script type="text/javascript" src="jquery-2.2.3.min.js"></script>
+  //<object class="PrismSparkle_circle" type="image/svg+xml" data="prismcircle.svg" width="300" height="300" style="position:absolute; left:200px; top:200px;"> </object>
+  $("body").prepend("<object class=\"PrismSparkle_circle\" type=\"image/svg+xml\" data=\"view-source:http://chiudesu.konjiki.jp/PrismTest/prismcircle.svg\" width=\"300\" height=\"300\" style=\"position:absolute; left:200px; top:200px;\"> </object>");
 
-    //要素取得
-    var svgelms = document.getElementsByClassName("PrismSparkle_circle");
-    var circles = new Array(svgelms.length);
-    for (var i = 0; i < circles.length; i++) {
-        circles[i] = $(svgelms[i].contentDocument).find("svg").find("circle");
-    };
-    $circles = $(circles);
-    cntf = 0.0;
-    tick = setInterval(sparkle, 1000 / FPS);
+  //要素取得
+  var svgelms = document.getElementsByClassName("PrismSparkle_circle");
+  var circles = new Array(svgelms.length);
+  for (var i = 0; i < circles.length; i++) {
+    circles[i] = $(svgelms[i].contentDocument).find("svg").find("circle");
+  };
+  $circles = $(circles);
+  cntf = 0.0;
+  tick = setInterval(sparkle, 1000 / FPS);
 }
 
 function stopSparkle() {
-    clearInterval(tick);
+  console.log("stopSparkle");
+  clearInterval(tick);
 }
 
 chrome.runtime.onMessage.addListener(function(req, sender, sendRes) {
-    console.log("getMessage");
-    switch (req.type) {
-        case "PrismSparkle_start":
-            startSparkle();
-            sendRes({res:"happy"});
-            break;
-        case "PrismSparkle_stop":
-            stopSparkle();
-            sendRes({res:"naru"});
-            break;
-    }
+  console.log("getMessage");
+  switch (req.type) {
+    case "PrismSparkle_start":
+      startSparkle();
+      sendRes({
+        success: true
+      });
+      break;
+    case "PrismSparkle_stop":
+      stopSparkle();
+      sendRes({
+        success: true
+      });
+      break;
+  }
 });
